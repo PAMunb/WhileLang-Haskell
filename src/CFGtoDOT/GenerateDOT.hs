e59ace2ff07b57158ee80de1957726d0821c7f7b
Lucas Dalle <lucasdalle@live.com>
2023-05-18 22:02:08 -0300

    add initial cfg to dot converter

---
+++ src/CFGtoDOT/GenerateDOT.hs
@@ -0,0 +1,40 @@
+module CFGtoDOT.GenerateDOT where
+
+import Syntax
+import CFG
+
+import Prelude hiding (init)
+import Data.Set 
+import Data.Foldable
+import Data.Map
+import Data.Text hiding (init)
+import Data.Text.IO
+import Data.List hiding (init)
+import System.Process
+
+writePairsToFile :: (Show k, Show v) => [(k, v)] -> IO ()
+writePairsToFile pairs = do
+  let content = generateDotFileContent pairs
+  Data.Text.IO.writeFile "cfg.dot" content
+  executeDotCommand
+
+generateDotFileContent :: (Show k, Show v) => [(k, v)] -> Data.Text.Text
+generateDotFileContent pairs =
+  Data.Text.unlines $
+    [ Data.Text.pack "digraph {"
+    , Data.Text.pack "\tlabel = <<font color='red'><b>CFG</b></font>>;"
+    , Data.Text.pack "\tlabelloc = \"t\";"
+    ] ++
+    Prelude.map (\(key, value) -> Data.Text.concat [Data.Text.pack "\t", Data.Text.pack $ show key, Data.Text.pack " -> ", Data.Text.pack $ show value]) pairs ++
+    [ Data.Text.pack "}"
+    ]
+
+executeDotCommand :: IO ()
+executeDotCommand = do
+  _ <- system "dot -Tpng cfg.dot -o cfg.png"
+  pure ()
+  
+callWritePairsToFile :: IO ()
+callWritePairsToFile = do
+  let myPairs = Data.Set.toList(flow factorial) -- alterar referência
+  writePairsToFile myPairs
