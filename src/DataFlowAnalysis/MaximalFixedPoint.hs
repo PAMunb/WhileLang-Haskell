565c4e75ffb1356454778c71176ea51aa8cdc9c4
Bruno Almeida da Silva <eng.fis.bruno@gmail.com>
2023-05-22 22:54:57 -0300

    Create getters for RD and LV

--- src/DataFlowAnalysis/MaximalFixedPoint.hs
+++ src/DataFlowAnalysis/MaximalFixedPoint.hs
@@ -8,21 +8,29 @@ import DataFlowAnalysis.LiveVariables
 
 import Data.Set
 
-data Analisys = RD DataFlowAnalysis.ReachingDefinitions.RD
+data Analysis = RD DataFlowAnalysis.ReachingDefinitions.RD
               | LV DataFlowAnalysis.LiveVariables.LV
 
-data AnalisysEntry = RDEntry DataFlowAnalysis.ReachingDefinitions.RDEntry
+data AnalysisEntry = RDEntry DataFlowAnalysis.ReachingDefinitions.RDEntry
                    | LVEntry DataFlowAnalysis.LiveVariables.LVEntry
 
-data AnalisysExit = RDExit
+data AnalysisExit = RDExit
                   | LVExit
 
-mapping :: Label -> Program -> AnalisysEntry -> Analisys
+getRD :: Analysis -> RD
+getRD (RD rd) = rd
+getRD (LV _) = empty
+
+getLV :: Analysis -> LV
+getLV (RD _) = empty
+getLV (LV lv) = lv
+
+mapping :: Label -> Program -> AnalysisEntry -> Analysis
 mapping l p (RDEntry rd) = RD (DataFlowAnalysis.ReachingDefinitions.rdExit l p rd)
 mapping l p (LVEntry lv) = LV (DataFlowAnalysis.LiveVariables.lvExit l p lv)
 
-lFunc :: Maybe Program -> RD
-lFunc (Just p) = Data.Set.fromList [(v, DataFlowAnalysis.ReachingDefinitions.uninitialized) | v <- Data.Set.toList (CFG.getVars p) ]
-lFunc Nothing = Data.Set.empty
+lFunc :: Maybe Program -> Analysis
+lFunc (Just p) = RD (Data.Set.fromList [(v, DataFlowAnalysis.ReachingDefinitions.uninitialized) | v <- Data.Set.toList (CFG.getVars p) ])
+lFunc Nothing = LV (Data.Set.empty)
 
--- initializeMFP :: Set (Label, Label) -> Set Label -> Analisys
+-- initializeMFP :: Set (Label, Label) -> Set Label -> Analysis
