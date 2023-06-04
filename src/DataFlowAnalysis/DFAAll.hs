dc594e91f2b642ea81042c05241ae994f4855154
Bruno Almeida da Silva <eng.fis.bruno@gmail.com>
2023-06-03 21:41:05 -0300

    Adds incomplete framework generalization

--- src/DataFlowAnalysis/DFAAll.hs
+++ src/DataFlowAnalysis/DFAAll.hs
@@ -7,41 +7,44 @@ import DataFlowAnalysis.LiveVariables
 import DataFlowAnalysis.ReachingDefinitions
 import DataFlowAnalysis.AvailableExpressions
 import DataFlowAnalysis.VeryBusyExpressions
+import DataFlowAnalysis.Framework
 
 import Data.Set
 import Data.Map
 
-dfa :: (Ord a) => Program -> (Blocks -> Program -> (Set a)) -> (Blocks -> Program -> (Set a)) -> Bool -> Bool ->  (Data.Map.Map Label (Set a), Data.Map.Map Label (Set a))
-dfa prog gen kill isForward isMay = chaoticIteration f initial
+dfa :: Program -> (Analysis, AnalysisInverse) -> (AnalysisOutputType, AnalysisOutputType)
+dfa prog initial = chaoticIteration f initial
   where
-    initial = (Data.Map.empty, Data.Map.empty)
     f mappings = Data.Set.foldr updateMappings mappings (labels prog)
-    updateMappings l (entry', exit') =
-      let 
-          newEntry = if isForward then combinator l prog exit' isForward isMay else  generator l prog exit' gen kill
-          newExit = if isForward then generator l prog entry' gen kill else combinator l prog entry' isForward isMay
-      in (Data.Map.insert l newEntry entry', Data.Map.insert l newExit exit')
-
-combinator :: (Ord a) => Label -> Program -> (Data.Map.Map Label (Set a)) -> Bool -> Bool -> Set a
-combinator l prog mapToCombine isForward isMay
-  | l == CFG.init prog = Data.Set.empty
-  | otherwise = if isMay then Data.Set.unions sets else intersections sets where
-    cfg = if isForward then flow prog else flowR prog
-    sets = [findWithDefault Data.Set.empty l' mapToCombine | (l', label) <- Data.Set.toList(cfg), label == l]
-
-generator :: (Ord a) => Label -> Program -> (Data.Map.Map Label (Set a)) -> (Blocks -> Program -> (Set a)) -> (Blocks -> Program -> (Set a)) -> Set a
-generator l prog mapToCombine gen kill = do
-    let Just b = findBlock l prog
-    ((findWithDefault Data.Set.empty l mapToCombine) `Data.Set.difference` (kill b prog)) `Data.Set.union` (gen b prog)
+    updateMappings l (analysis', analysisInv') =
+      let
+          newAnalysisItem = analysis l prog analysisInv'
+          newAnalysisInvItem = fFunc l prog analysis'
+          newAnalysisOut = Data.Map.insert l newAnalysisItem
+          newAnalysisInvOut = Data.Map.insert l newAnalysisInvItem
+      in (newAnalysisOut Data.Map.union analysis', newAnalysisInvOut Data.Map.union analysisInv')
 
 dfa_reachingDefinitions :: Program -> (RDEntry, RDExit)
-dfa_reachingDefinitions prog = dfa prog genRD killRD True True
+dfa_reachingDefinitions prog = getRD (dfa prog (initialize (external prog (RD Data.Map.empty)) (lFunc prog (RD Data.Map.empty))) (emptyAnalysisInverse (RDInv Data.Map.empty)))
 
-dfa_liveVariables :: Program -> (LVEntry, LVExit)
-dfa_liveVariables prog = dfa prog genLV killLV False True
+-- dfa_liveVariables :: Program -> (LVEntry, LVExit)
+-- dfa_liveVariables prog = dfa prog genLV killLV False True
+-- 
+-- dfa_availableExpressions :: Program -> (AEEntry, AEExit)
+-- dfa_availableExpressions prog = dfa prog genAE killAE True False
+-- 
+-- dfa_veryBusyExpressions :: Program -> (VBEntry, VBExit)
+-- dfa_veryBusyExpressions prog = dfa prog genVB killVB False False
 
-dfa_availableExpressions :: Program -> (AEEntry, AEExit)
-dfa_availableExpressions prog = dfa prog genAE killAE True False
+-- combinator :: (Ord a) => Label -> Program -> (Data.Map.Map Label (Set a)) -> Bool -> Bool -> Set a
+-- combinator l prog mapToCombine isForward isMay
+--   | l == CFG.init prog = Data.Set.empty
+--   | otherwise = if isMay then Data.Set.unions sets else intersections sets where
+--     cfg = if isForward then flow prog else flowR prog
+--     sets = [findWithDefault Data.Set.empty l' mapToCombine | (l', label) <- Data.Set.toList(cfg), label == l]
+-- 
+-- generator :: (Ord a) => Label -> Program -> (Data.Map.Map Label (Set a)) -> (Blocks -> Program -> (Set a)) -> (Blocks -> Program -> (Set a)) -> Set a
+-- generator l prog mapToCombine gen kill = do
+--     let Just b = findBlock l prog
+--     ((findWithDefault Data.Set.empty l mapToCombine) `Data.Set.difference` (kill b prog)) `Data.Set.union` (gen b prog)
 
-dfa_veryBusyExpressions :: Program -> (VBEntry, VBExit)
-dfa_veryBusyExpressions prog = dfa prog genVB killVB False False
--- src/DataFlowAnalysis/Framework.hs
+++ src/DataFlowAnalysis/Framework.hs
@@ -11,6 +11,7 @@ import DataFlowAnalysis.VeryBusyExpressions
 import DataFlowAnalysis.AvailableExpressions
 
 import Data.Set
+import Data.Map
 
 data AnalysisOutputType = RDOut DataFlowAnalysis.ReachingDefinitions.RD
                         | LVOut DataFlowAnalysis.LiveVariables.LV
@@ -27,21 +28,49 @@ data AnalysisInverse = RDInv DataFlowAnalysis.ReachingDefinitions.RDExit
                      | AEInv DataFlowAnalysis.AvailableExpressions.AEExit
                      | VBInv DataFlowAnalysis.VeryBusyExpressions.VBEntry
 
-getRD :: AnalysisOutputType -> DataFlowAnalysis.ReachingDefinitions.RD
-getRD (RDOut rd) = rd
-getRD _ = empty
+-- getRDOut :: AnalysisOutputType -> DataFlowAnalysis.ReachingDefinitions.RD
+-- getRDOut (RDOut rd) = rd
+-- getRDOut _ = Data.Set.empty
+
+getRD :: (Analysis, AnalysisInverse) -> (DataFlowAnalysis.ReachingDefinitions.RDEntry, DataFlowAnalysis.ReachingDefinitions.RDExit)
+getRD (RD rd, RDInv rdInv) = (rd, rdInv)
+getRD _ = (Data.Map.empty, Data.Map.empty)
 
 getLV :: AnalysisOutputType -> DataFlowAnalysis.LiveVariables.LV
 getLV (LVOut lv) = lv
-getLV _ = empty
+getLV _ = Data.Set.empty
 
 getAE :: AnalysisOutputType -> DataFlowAnalysis.AvailableExpressions.AE
 getAE (AEOut ae) = ae
-getAE _ = empty
+getAE _ = Data.Set.empty
 
 getVB :: AnalysisOutputType -> DataFlowAnalysis.VeryBusyExpressions.VB
 getVB (VBOut vb) = vb
-getVB _ = empty
+getVB _ = Data.Set.empty
+
+initialize :: Set Label -> AnalysisOutputType -> Analysis
+initialize ext (RDOut initial) = (RD (Data.Map.fromList [(k, initial) | k <- Data.Set.toList ext]))
+initialize ext (LVOut initial) = (LV (Data.Map.fromList [(k, initial) | k <- Data.Set.toList ext]))
+initialize ext (AEOut initial) = (AE (Data.Map.fromList [(k, initial) | k <- Data.Set.toList ext]))
+initialize ext (VBOut initial) = (VB (Data.Map.fromList [(k, initial) | k <- Data.Set.toList ext]))
+
+analysisConverter :: [(Label, AnalysisOutputType)] -> Analysis
+analysisConverter [(l, (RDOut rd))] = (RD (Data.Map.fromList [(l, rd)]))
+analysisConverter [(l, (LVOut lv))] = (LV (Data.Map.fromList [(l, lv)]))
+analysisConverter [(l, (AEOut ae))] = (AE (Data.Map.fromList [(l, ae)]))
+analysisConverter [(l, (VBOut vb))] = (VB (Data.Map.fromList [(l, vb)]))
+
+analysisInvConverter :: [(Label, AnalysisOutputType)] -> AnalysisInverse
+analysisInvConverter [(l, (RDOut rd))] = (RDInv (Data.Map.fromList [(l, rd)]))
+analysisInvConverter [(l, (LVOut lv))] = (LVInv (Data.Map.fromList [(l, lv)]))
+analysisInvConverter [(l, (AEOut ae))] = (AEInv (Data.Map.fromList [(l, ae)]))
+analysisInvConverter [(l, (VBOut vb))] = (VBInv (Data.Map.fromList [(l, vb)]))
+
+emptyAnalysisInverse :: AnalysisInverse -> AnalysisInverse
+emptyAnalysisInverse (RDInv _) = (RDInv Data.Map.empty)
+emptyAnalysisInverse (LVInv _) = (LVInv Data.Map.empty)
+emptyAnalysisInverse (AEInv _) = (AEInv Data.Map.empty)
+emptyAnalysisInverse (VBInv _) = (VBInv Data.Map.empty)
 
 external :: Program -> Analysis -> Set Label
 external p (RD _) = Data.Set.singleton (CFG.init p)
@@ -57,7 +86,9 @@ flow p (VB _) = CFG.flowR p
 
 lFunc :: Program -> Analysis -> AnalysisOutputType
 lFunc p (RD _) = RDOut (Data.Set.fromList [(v, DataFlowAnalysis.ReachingDefinitions.uninitialized) | v <- Data.Set.toList (DataFlowAnalysis.Helpers.fv p) ])
-lFunc _ _= LVOut (Data.Set.empty)
+lFunc _ (LV _)= LVOut Data.Set.empty
+lFunc _ (AE _)= AEOut Data.Set.empty
+lFunc _ (VB _)= VBOut Data.Set.empty
 
 fFunc :: Label -> Program -> Analysis -> AnalysisOutputType
 fFunc l p (RD rd) = RDOut (DataFlowAnalysis.ReachingDefinitions.rdExit l p rd)
